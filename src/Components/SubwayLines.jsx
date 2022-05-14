b21d5d4b3868dc6b7ffcf11f68b2bc98661e31a4
Sean McNamara <seanmcnamara33@gmail.com>
2022-05-13 17:10:55 -0700

    add default value to subway lines section

--- src/Components/SubwayLines.jsx
+++ src/Components/SubwayLines.jsx
@@ -5,6 +5,7 @@ const SubwayLines = ({allRoutes, onLineSelectChange}) => {
     <div className='line-select'>
     <h2 className='section-title'>Choose Your Line</h2>
     <select name='lines' id='lines' onChange={(event) => onLineSelectChange(event)}>
+      <option value='' disabled seleced hidden>---Rapid Transit Lines---</option>
       {allRoutes.map((line) => {
         return <option value={line.id} key={line.id}>ID: {line.id}, NAME: {line.attributes.long_name}</option>
       })}
